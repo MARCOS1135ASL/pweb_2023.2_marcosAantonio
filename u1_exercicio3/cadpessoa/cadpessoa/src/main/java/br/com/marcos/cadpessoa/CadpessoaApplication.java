ad383eecaff88e87c110a43215d550b43753357a
MARCOS1135ASL <mlima3511@gmail.com>
2024-03-10 12:12:59 -0300

    ux3

---
+++ u1_exercicio3/cadpessoa/cadpessoa/src/main/java/br/com/marcos/cadpessoa/CadpessoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.cadpessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoaApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/cadpessoa/cadpessoa/src/test/java/br/com/marcos/cadpessoa/CadpessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.cadpessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
